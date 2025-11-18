8d8f9a7e05aba89cff1a1d869e0dffd288b4a402
dryhour <derekp9210@gmail.com>
2025-11-18 18:35:01 -0500

    fix: reversed cursor colors

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -2,30 +2,30 @@ const coords = { x: 0, y: 0};
 const circles = document.querySelectorAll(".circle");
 
 const colors = [
-    '#4260f5',
-    '#3e59e2',
-    '#3a52cf',
-    '#364abc',
-    '#3243a9',
-    '#2e3b96',
-    '#2a3483',
-    '#262d70',
-    '#22265d',
-    '#1e1f4a',
-    '#1a1737',
-    '#161024',
-    '#120811',
-    '#0e0000',
-    '#0a0000',
+    '#000000',
     '#050000',
-    '#000000'
+    '#0a0000',
+    '#0e0000',
+    '#120811',
+    '#161024',
+    '#1a1737',
+    '#1e1f4a',
+    '#22265d',
+    '#262d70',
+    '#2a3483',
+    '#2e3b96',
+    '#3243a9',
+    '#364abc',
+    '#3a52cf',
+    '#3e59e2',
+    '#4260f5'
 ];
+
 
 circles.forEach(function(circle, i){
     circle.x = 0;
     circle.y = 0;
-    const reversedColors = colors.length - 1 - (i % colors.length);
-    circle.style.backgroundColor = reversedColors[i % colors.length];
+    circle.style.backgroundColor = colors[i % colors.length];
 });
 
 window.addEventListener("mousemove", function(e){
