4f8026e043676f2663eabf733ca907941c3b8ad9
dryhour <derekp9210@gmail.com>
2025-11-24 21:37:49 -0500

    chore: replace with inpkg

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,30 +1,24 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "https://unpkg.com/three@0.129.0/build/three.module.js";
+import { GLTFLoader } from "https://unpkg.com/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+let object;
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
-let object;
+const objToRender = "black_hole";
 
-let objToRender = 'black_hole';
 const loader = new GLTFLoader();
-
 loader.load(
   `./models/${objToRender}/scene.gltf`,
-  function (gltf) {
+  (gltf) => {
     object = gltf.scene;
     scene.add(object);
   },
-  function (xhr) {
-    if (xhr.total) {
-      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    }
+  (xhr) => {
+    if (xhr.total) console.log((xhr.loaded / xhr.total * 100) + "% loaded");
   },
-  function (error) {
-    console.error(error);
-  }
+  (error) => console.error(error)
 );
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -35,7 +29,6 @@ camera.position.z = 10;
 
 const topLight = new THREE.DirectionalLight(0xffffff, 1);
 topLight.position.set(500, 500, 500);
-topLight.castShadow = true;
 scene.add(topLight);
 
 const ambientLight = new THREE.AmbientLight(0x333333, 1);
@@ -43,24 +36,22 @@ scene.add(ambientLight);
 
 function animate() {
   requestAnimationFrame(animate);
-
   if (object) {
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+    object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
+    object.rotation.x = -1.2 + (mouseY / window.innerHeight) * 2.5;
   }
-
   renderer.render(scene, camera);
 }
 
-window.addEventListener("resize", function () {
+window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-document.onmousemove = (e) => {
+document.addEventListener("mousemove", (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
-}
+});
 
 animate();
