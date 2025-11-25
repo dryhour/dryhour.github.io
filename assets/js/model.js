ce0014801e9c60f614109a67d3a4884820af59e7
dryhour <derekp9210@gmail.com>
2025-11-25 11:44:49 -0500

    fix: 3d model

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,56 +1,40 @@
-import * as THREE from "https://unpkg.com/three@0.129.0/build/three.module.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdddddd);
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-let object;
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
+camera.position.set(2, 2, 5);
 
-const loader = new GLTFLoader();
-loader.load(
-  './models/ball_simple/scene.gltf',
-  (gltf) => {
-    object = gltf.scene;
-    scene.add(object);
-  },
-  (xhr) => {
-    if (xhr.total) console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  (error) => console.error(error)
-);
-
-const renderer = new THREE.WebGLRenderer({ alpha: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('container3D').appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
-camera.position.z = 10;
+window.addEventListener('resize', onWindowResize, false);
 
-const topLight = new THREE.DirectionalLight(0xffffff, 1);
-topLight.position.set(500, 500, 500);
-scene.add(topLight);
-
-const ambientLight = new THREE.AmbientLight(0x333333, 1);
-scene.add(ambientLight);
-
-function animate() {
-  requestAnimationFrame(animate);
-  if (object) {
-    object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
-    object.rotation.x = -1.2 + (mouseY / window.innerHeight) * 2.5;
-  }
-  renderer.render(scene, camera);
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+const ambientLight = new THREE.AmbientLight(0x404040, 2);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(0, 10, 0);
+scene.add(directionalLight);
+
+const modelName = 'ball_simple'
+const loader = new THREE.GLTFLoader();
+const gltfFilePath = `./models/${modelName}/scene.gltf`;
+
+loader.load(gltfFilePath, function (gltf) {
+    scene.add(gltf.scene);
+}, undefined, function (error) {
+    console.error('An error happened while loading the model:', error);
 });
 
-document.addEventListener('mousemove', (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-});
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
 
 animate();
